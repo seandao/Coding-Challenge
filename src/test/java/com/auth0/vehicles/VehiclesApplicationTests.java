d0a83e3e42f96ec75d9cfafb1bfcde06b1e324ac
Sean Dao <seandao2011@gmail.com>
2020-11-01 18:11:09 -0800

    First commit

---
+++ src/test/java/com/auth0/vehicles/VehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth0.vehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
